03512c672f316588206b6bd59b3687c49710fefc
ingaresa <resa.inga@gmail.com>
2023-09-06 20:53:38 +0700

    commit test

---
+++ nodejs/helper-latihan.js
@@ -0,0 +1,14 @@
+// common js
+// cara1
+// const showAtConsole = (message) => {
+//     console.log (message);
+// }
+// module.export = {showAtConsole};
+
+//  cara2
+exports.showAtConsole = (message) => {
+    console.log (message);
+}
+
+// klo mau ubah ke es6, di point ke perintah aktifnya sampe ada 
+// tulisan quick fix
---
+++ nodejs/index-latihan.js
@@ -0,0 +1,9 @@
+const {showAtConsole} = require ("./helper-latihan");
+
+showAtConsole("test helper");
+
+console.log ("Hello World!");
+
+alert();
+// alert nggak terdefinisi di node karena alert ini kaitannya
+// sama browser
